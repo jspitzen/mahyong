089da4cfb79d42864461618fec8eac6b6751f264
= <jorick1810@gmail.com>
2014-02-16 17:15:09 +0100

    Game, Round and PlayerRound models registered

--- mahyong_game/admin.py
+++ mahyong_game/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from mahyong_game.models import Player, Family
+from mahyong_game.models import Player, Family, Game, Round, PlayerRound
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Family)
+admin.site.register(Game)
+admin.site.register(Round)
+admin.site.register(PlayerRound)
