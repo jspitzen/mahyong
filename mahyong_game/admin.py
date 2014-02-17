c20b33d4b4a52c21f85bace9406143ae7ef18bee
= <jorick1810@gmail.com>
2014-02-17 12:18:27 +0100

    Implemented several admin display classes

--- mahyong_game/admin.py
+++ mahyong_game/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 
 from mahyong_game.models import Player, Family, Game, Round, PlayerRound
 
+class PlayerRoundInline(admin.StackedInline):
+    model = PlayerRound
+    extra = 0
+
+class RoundInline(admin.TabularInline):
+    model = Round
+
+class PlayerAdmin(admin.ModelAdmin):
+    inlines = [PlayerRoundInline]
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [RoundInline]
+
 # Register your models here.
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(Family)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Round)
 admin.site.register(PlayerRound)
