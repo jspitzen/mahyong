9a99bc0c595c2bb716d8d1f51fec61dd22bbc03d
= <jorick1810@gmail.com>
2014-02-18 12:42:56 +0100

    Views implemented

--- mahyong/urls.py
+++ mahyong/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mahyong.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('mahyong_game.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mahyong_game/views.py
+++ mahyong_game/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.http import HttpResponse
 
-# Create your views here.
+from mahyong_game.models import *
+
+class IndexView(generic.ListView):
+    template_name = 'mahyong_game/index.html'
+    context_object_name = 'active_games'
+
+    def get_queryset(self):
+        """Return all active games"""
+        return Game.objects.all()
