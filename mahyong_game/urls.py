4a66811f02fbdca4cb0647c0e71e0e195e029199
= <jorick1810@gmail.com>
2014-02-28 20:10:16 +0100

    GameDetailView implemented

--- mahyong_game/urls.py
+++ mahyong_game/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from mahyong_game import views
 
 urlpatterns = patterns('',
+    url(r'^game/(?P<pk>\d+)/$', views.GameDetailView.as_view(), name='index'),
     url(r'^$', views.IndexView.as_view(), name='index'),
 )
--- mahyong_game/views.py
+++ mahyong_game/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.http import HttpResponse
+from django.utils import timezone
 
 from mahyong_game.models import *
 
 class IndexView(generic.ListView):
     template_name = 'mahyong_game/index.html'
-    context_object_name = 'active_games'
+    context_object_name = 'games'
 
     def get_queryset(self):
         """Return all active games"""
         return Game.objects.all()
+
+class GameDetailView(generic.DetailView):
+    model = Game
+    context_object_name = 'object_set'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['menu']   = Game.objects.all()
+        context['active'] = super(GameDetailView, self).get_context_data(**kwargs)
+        context['now']    = timezone.now()
+        return context
