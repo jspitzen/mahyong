c4e9d8d3bc05b2ecd63288a9d9284a25a39437a4
= <jorick1810@gmail.com>
2014-02-28 20:09:10 +0100

    Universal redirect from url to mahyong_game module

--- mahyong/urls.py
+++ mahyong/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('mahyong_game.urls')),
+    url(r'^', include('mahyong_game.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
