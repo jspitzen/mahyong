de6d98913f59b20e2e040676a68685d241aa0b6f
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2018-09-18 09:34:38 +0200

    [IMP] - #1 - Update patterns to new Django syntax

--- mahyong_game/urls.py
+++ mahyong_game/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from mahyong_game import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^game/(?P<pk>\d+)/$', views.GameDetailView.as_view(), name='index'),
     url(r'^player/(?P<pk>\d+)/$', views.PlayerDetailView.as_view(), name='index'),
     url(r'^$', views.GameIndexView.as_view(), name='index'),
     url(r'^game/$', views.GameIndexView.as_view(), name='index'),
     url(r'^player/$', views.PlayerIndexView.as_view(), name='index'),
-)
+]
